398be1e351980142da6c91f15280022b399073f8
samjmccormick <samjmccormick@gmail.com>
2024-12-18 12:02:25 -0800

    working

--- index.js
+++ index.js
@@ -2,23 +2,32 @@ const http = require("http");
 const url = require("url");
 const fs = require("fs");
 
+const pages = ["./about.html", "./contact-me.html", "./index.html"];
+
 http
   .createServer(function (req, res) {
     let q = url.parse(req.url, true);
     let filename = "." + q.pathname;
 
-    fs.readFile(filename, function (err, data) {
-      console.log(filename);
-      if (err) {
-        fs.readFile("./404.html", function (err, data) {
-          res.writeHead(404, { "Content-Type": "text/html" });
-          res.write(data);
-          return res.end();
-        });
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
+    //checks if the url path matches any of the pages, if so it reads the correct file, else reads the 404 file
+    //just throwing errors for handling
+
+    if (pages.includes(filename.toString())) {
+      fs.readFile(filename, function (err, data) {
+        if (err) throw err;
+
+        res.writeHead(200, { "content-type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else {
+      fs.readFile("./404.html", function (err, data) {
+        if (err) throw err;
+
+        res.writeHead(404, { "content-type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    }
   })
   .listen(8080);
